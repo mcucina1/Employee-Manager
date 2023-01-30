60667635c83c0d6b244013c1b28db76f486cad9a
w45272 <w45272@rams.colostate.edu>
2023-01-29 19:05:21 -0700

    Add tests for Project ctor, getName, and getSize

--- server/src/test/java/edu/colostate/cs415/model/ProjectTest.java
+++ server/src/test/java/edu/colostate/cs415/model/ProjectTest.java
@@ -1,10 +1,65 @@
 package edu.colostate.cs415.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+
 public class ProjectTest {
+	String firstDescription;
+	String secondDescription;
+	String testProjectName;
+
+	Qualification firstQualification;
+	Qualification secondQualification;
+
+	Set<Qualification> qualifications;
+
+	Project testProject;
+
+	@Before
+	public void setUp() {
+		firstDescription = "Qualified Engineer";
+		secondDescription = "Qualified Scientist";
+		testProjectName = "Test Project";
+
+		firstQualification = new Qualification(firstDescription);
+		secondQualification = new Qualification(secondDescription);
+
+		Set<Qualification> testQualifications = new HashSet<Qualification>();
+		testQualifications.add(firstQualification);
+		testQualifications.add(secondQualification);
+
+		testProject = new Project(testProjectName, testQualifications, ProjectSize.SMALL);
+	}
+
 	@Test
 	public void test() {
 		assert (true);
+	}
+
+	@Test
+	public void testProjectConstructor() {
+		assert(true);
+	}
+
+	@Test
+	public void testGetName() {
+		assertEquals(testProject.getName(), "Test Project");
+	}
+
+	@Test
+	public void testGetSize() {
+		assertEquals(testProject.getSize(), ProjectSize.SMALL);
+	}
+
+	@Test
+	public void testProjectQualifications() {
+		assertEquals(firstQualification, new Qualification(firstDescription));
+		assertEquals(secondQualification, new Qualification(secondDescription));
 	}
 }
