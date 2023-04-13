abfbd710c606f9f2baabfa6163ccbb4be967169b
mrzlowe <zachary.lowe@colostate.edu>
2023-04-13 16:50:54 -0600

    Added asserts and verified FINISHED

--- server/src/test/java/edu/colostate/cs415/server/RestControllerTest.java
+++ server/src/test/java/edu/colostate/cs415/server/RestControllerTest.java
@@ -24,6 +24,7 @@ import edu.colostate.cs415.dto.AssignmentDTO;
 import edu.colostate.cs415.model.Company;
 import edu.colostate.cs415.model.Project;
 import edu.colostate.cs415.model.ProjectSize;
+import edu.colostate.cs415.model.ProjectStatus;
 import edu.colostate.cs415.model.Qualification;
 import edu.colostate.cs415.model.Worker;
 
@@ -256,7 +257,8 @@ public class RestControllerTest {
         assertTrue(company.getEmployedWorkers().isEmpty());
         company.createQualification("Qualification 1");
         company.createQualification("Qualification 2");
-        company.createProject("Project 1", company.getQualifications(), ProjectSize.SMALL);
+        Project temp = company.createProject("Project 1", company.getQualifications(), ProjectSize.SMALL);
+        temp.setStatus(ProjectStatus.ACTIVE);
         company.createWorker("WonderWoman", company.getQualifications(), 186000.0);
         assertFalse(company.getProjects().isEmpty());
         assertFalse(company.getQualifications().isEmpty());
@@ -271,6 +273,7 @@ public class RestControllerTest {
                 .asString();
 
         assertEquals("\"OK\"", responseString);
+        assertTrue(temp.getStatus().equals(ProjectStatus.FINISHED));
     }
 
     @Test(expected = IOException.class)
