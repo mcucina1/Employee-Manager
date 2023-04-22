134858c3d785cec5c3df439448f58da8cfa6c999
Christian Carnahan <christian@thecarnahans.com>
2023-04-22 09:20:06 -0600

    Added npm installs and implemented beginning stages of Workers page.

--- client/src/pages/workers.js
+++ client/src/pages/workers.js
@@ -1,12 +1,42 @@
-import { pageStyle } from '../utils/styles'
+import { getWorkers } from '../services/dataService'
+import { Container} from 'reactstrap';
+import { useEffect, useState } from 'react'
+import ClickList from '../components/ClickList'
+import LocationID from '../utils/location'
+import { darkGrayContainerStyle, grayContainerStyle, pageStyle } from '../utils/styles'
+
+const Worker = (worker, active) => {
+    return (
+        <div>
+            <div>{worker.name}</div>
+            {active === true ? WorkerBody(worker) : null}
+        </div>
+    )
+}
+
+const WorkerBody = (worker) => {
+    return (
+        <div style={grayContainerStyle}>
+            Qualifications: <ClickList list={worker.qualifications} styles={darkGrayContainerStyle} path="/qualifications" /> {"\n"}
+            Salary: {worker.salary}{"\n"}
+            Projects: <ClickList list={worker.projects} styles={darkGrayContainerStyle} path="/projects" /> {"\n"}
+        </div>
+    )
+}
 
 const Workers = () => {
+    const [workers, setWorkers] = useState([])
+    useEffect(() => { getWorkers().then(setWorkers) }, [])
+    const active = LocationID('workers', workers, 'name')
     return (
-        <div style={pageStyle}>
-            <h1>
-                This page displays a table containing all the workers.
-            </h1>
-        </div>
+        <Container>
+            <div style={pageStyle}>
+                <h1>
+                    This page displays a table containing all the qualifications.
+                </h1>
+                <ClickList active={active} list={workers} item={Worker} path='/qualifications' id='description' />
+            </div>
+        </Container>
     )
 }
 
