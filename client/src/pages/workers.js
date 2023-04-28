03984b1fc509cd5a9fc1910a3758db153bd5b170
w45272 <w45272@rams.colostate.edu>
2023-04-28 17:14:47 -0600

    trying a billion things

--- client/src/pages/workers.js
+++ client/src/pages/workers.js
@@ -1,9 +1,10 @@
-import { getWorkers } from '../services/dataService'
-import { Container} from 'reactstrap';
-import { useEffect, useState } from 'react'
+import { getWorkers, createWorker, getQualifications } from '../services/dataService'
+import { Container } from 'reactstrap';
+import { useEffect, useState, useRef } from 'react'
 import ClickList from '../components/ClickList'
 import LocationID from '../utils/location'
 import { darkGrayContainerStyle, grayContainerStyle, pageStyle } from '../utils/styles'
+import Select from 'react-select'
 
 const Worker = (worker, active) => {
     return (
@@ -33,15 +34,66 @@ const Workers = () => {
     const [workers, setWorkers] = useState([])
     useEffect(() => { getWorkers().then(setWorkers) }, [])
     const active = LocationID('workers', workers, 'name')
+
     return (
         <Container>
             <div style={pageStyle}>
                 <h1>
                     This page displays a table containing all the workers.
                 </h1>
+                <PostWorker setWorkers={setWorkers} />
                 <ClickList active={active} list={workers} item={Worker} path='/workers' id='name' />
             </div>
         </Container>
+    )
+}
+
+const PostWorker = (props) => {
+    const [name, setName] = useState("")
+    const [qualifications, setQualifications] = useState([]);
+    const [salary, setSalary] = useState(0);
+    const inputQualifications = useRef("");
+
+    useEffect(() => { getQualifications().then(setQualifications) }, [])
+    const selectStyle = {
+        margin-bottom: '10px',
+        margin-top: '10px',
+        font-family: 'cursive, sans-serif',
+        outline: '0',
+        background: '#2ecc71',
+        color: '#fff',
+        border: '1px solid crimson',
+        padding: '4px',
+        border-radius: '9px',
+    }
+    return (
+        <form onSubmit={async () => {
+            const worker = await createWorker({ name: name, qualifications: qualifications, salary: salary });
+            props.setWorkers([worker])
+        }}>
+            <label> Name
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </label>
+            <label> Qualifications
+            <select size="1" multiple>
+                {qualifications.map((qualification) => {
+                    return <option value={qualification.description}>{qualification.description}</option>;
+                })}
+            </select>
+            </label>
+            <label> Salary
+                <input
+                    type="text"
+                    value={salary}
+                    onChange={(e) => setSalary(e.target.value)}
+                />
+            </label>
+            <input type="submit" />
+        </form>
     )
 }
 
